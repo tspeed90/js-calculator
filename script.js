62831a7de3f3363b9ca6d702a416cc91becb7522
tspeed90 <tspeed90@gmail.com>
2018-01-04 22:04:40 +0000

    added event listener for input via keyboard

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ function displayCalculation() {
 
 let buttons = document.querySelectorAll(".number, .operator");
 
+document.addEventListener('keypress', function(e) {
+  if (/[0-9+\-*/=.]/.test(e.key)) {
+    stringToCalculate += e.key;
+    console.log(stringToCalculate);
+  }
+});
+
 for (let button of buttons) {
   button.addEventListener('click', appendInputCharacter);
 }
