a1814922da12f025f745444f36cabd0523882be3
tspeed90 <tspeed90@gmail.com>
2018-01-06 15:29:01 +0000

    implemented equal sign handling for keyboard and on click and added function to calculate sum

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let stringToCalculate = "0";
 displayCalculation();
+
 function appendInputCharacter(e) {
   if (stringToCalculate === "0") {
     stringToCalculate = "";
@@ -19,18 +20,33 @@ function removeFinalCharacter() {
   }
   displayCalculation();
 }
+//TODO: add to keyboard event listener AND to click event listener
+function calculate() {
+  let operators = /[+\-/*]/;
+  let values = stringToCalculate.split(operators);
+  stringToCalculate = values.reduce((sum, value) => {
+      value = parseInt(value, 10);
+      return sum + value;
+  }, 0);
+  stringToCalculate = stringToCalculate.toString();
+}
 
 let buttons = document.querySelectorAll(".number, .operator");
 
-document.addEventListener('keypress', function(e) {
-  if (stringToCalculate === "0") {
-    stringToCalculate = "";
-  }
-  if (/[0-9+\-*/=.]/.test(e.key)) {
+document.addEventListener('keyup', function(e) {
+  if (e.key === "Backspace" || e.keyCode === 8) {
+    removeFinalCharacter();
+  } else if (e.key === "=") {
+    console.log(stringToCalculate)
+    calculate();
+  } else if (/[0-9+\-*/=.]/.test(e.key)) {
+    if (stringToCalculate === "0") {
+      stringToCalculate = "";
+    }
     stringToCalculate += e.key;
-    displayCalculation();
     console.log(stringToCalculate);
   }
+  displayCalculation();
 });
 
 document.querySelector('.clear').addEventListener('click', function() {
@@ -40,12 +56,11 @@ document.querySelector('.clear').addEventListener('click', function() {
 
 document.querySelector('.delete').addEventListener('click', removeFinalCharacter);
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === "Backspace" || e.keyCode === 8) {
-    removeFinalCharacter();
-  }
-});
-
 for (let button of buttons) {
   button.addEventListener('click', appendInputCharacter);
 }
+
+document.querySelector('.equals').addEventListener('click', function() {
+  calculate();
+  displayCalculation();
+});
